8f07787d04c442103c11d6a210f788a8ed9c46ba
nachogarrone <nachogarrone@gmail.com>
2016-08-20 16:39:42 -0300

    Player view added

--- routes/player.js
+++ routes/player.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var midigenerator = require('midi-generator');
+var Parser = require("jison").Parser;
 
 var router = express.Router();
 
@@ -7,5 +7,35 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('player', { title: 'Midi JS Player' });
 });
+
+router.post('/', function (req, res) {
+    var parseResult = parser(req.body.source);
+    res.render('player', { title: 'Midi JS Player', parseResult: parseResult });
+});
+
+var grammar = {
+    "lex": {
+        "rules": [
+            ["\\s+", "/* skip whitespace */"],
+            ["[a-f0-9]+", "return 'HEX';"]
+        ]
+    },
+
+    "bnf": {
+        "hex_strings" :[ "hex_strings HEX",
+            "HEX" ]
+    }
+};
+
+function parser(source) {
+    if(source){
+        var parser = new Parser(grammar);
+
+        console.log('Parsing...');
+        var ret = parser.parse(source);
+        console.log(ret);
+        return ret;
+    }
+}
 
 module.exports = router;
